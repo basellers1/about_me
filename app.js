3ba0b57af8c923a6bd60f1a77be0d99cf0ca1c3f
Britt Anne Sellers <basellers1@yahoo.com>
2016-09-20 16:48:05 -0700

    This is my first solo project

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
+var username = prompt('What is your name?').toLowerCase();
+  alert('Welcome, ' + username + '!');
+    console.log('The user\'s name is ' + username);
+var hello = prompt('Do you want to play a game?').toLowerCase();
+  if (hello === 'Y' || hello === 'yes') {
+  alert('That\'s fantastic!');
+    console.log('That\'s fantastic!');
+}
+  else {
+  alert('Maybe another day.');
+    console.log('Maybe another day.');
+  }
+var q1 = prompt('I went to a private school in Hawaii, can you guess which one?');
+  if (useranswer = 'University of Hawaii'){
+  alert('Sorry, that is not correct. UH is a public University');
+    console.log('Sorry, that is not correct. UH is a public University');
+  }
+  else if (useranswer = 'Hawaii Pacific University') {
+  alert('Yes, that\'s the right answer!');
+  console.log('Yes, that\'s the right answer!');
+  }
+  else {
+    alert('Sorry, that is not correct.  That must be a new one.');
+    console.log('Sorry, that is not correct.  That must be a new one.');
+};
 
-var username = prompt('What is your name?');
-alert('Welcome, ' + username + '!');
-console.log('The user\'s name is ' + username);
-var age = prompt('How old are you?');
-alert('Wow ' + age + ' is young!')
-
-console.log('The user\'s name is ' + username + ' and their age is ' + age);
-var hobby = prompt("What is your favorite hobby?");
-alert('I like ' + hobby + ' too');
-console.log('The user\'s name is ' + username + ' and their age is ' + age + ' and their hobby is ' + hobby);
-var bedtime = prompt('What time do you go to bed?')
-alert("Well, you must get plenty of rest when you go to bed at " + bedtime + "");
-console.log('The user\'s bedtime is ' + bedtime);
-console.log('The user\'s name is ' + username + ' and their age is ' + age + ' and their hobby is ' + hobby + ' and their bedtime is ' + bedtime);
+var q2 = prompt('Memorable jobs for me were at Holland America Line, Hyatt Regency Waikiki. Can you guess my Major? ');
+  if (useranswer = 'Travel'){
+    alert('Good guess!  Actually, I got my B.S.B.A. degree with a Travel Industry Management Major.');
+    console.log('Good guess!  Actually, I got my B.S.B.A. degree with a Travel Industry Management Major.');
+}
+  else {
+    alert('Good guess but that is not correct. Actually, I got my B.S.B.A. degree with a Travel Industry Management Major.');
+    console.log('Good guess but that is not correct. Actually, I got my B.S.B.A. degree with a Travel Industry Management Major.');
+};
+var q3 = prompt('Do you know which forms of exercise I like?').toLowerCase();
+  if (hello === 'Y' || hello === 'yes') {
+  alert('That\'s fantastic!');
+  console.log('That\'s fantastic!');
+}
+else{
+  alert('I wouldn\'t expect you to.  I like yoga and walking');
+  console.log('I wouldn\'t expect you to.  I like yoga and walking');
+}
