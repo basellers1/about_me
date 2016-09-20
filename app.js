aed5372b8896f8c21d2bc7cee4f786c63dccd5c0
Britt Anne Sellers <basellers1@yahoo.com>
2016-09-20 12:16:03 -0700

    scaffold html, js, css files, delete old html file

---
+++ app.js
@@ -0,0 +1,15 @@
+
+var username = prompt('What is your name?');
+alert('Welcome, ' + username + '!');
+console.log('The user\'s name is ' + username);
+var age = prompt('How old are you?');
+alert('Wow ' + age + ' is young!')
+
+console.log('The user\'s name is ' + username + ' and their age is ' + age);
+var hobby = prompt("What is your favorite hobby?");
+alert('I like ' + hobby + ' too');
+console.log('The user\'s name is ' + username + ' and their age is ' + age + ' and their hobby is ' + hobby);
+var bedtime = prompt('What time do you go to bed?')
+alert("Well, you must get plenty of rest when you go to bed at " + bedtime + "");
+console.log('The user\'s bedtime is ' + bedtime);
+console.log('The user\'s name is ' + username + ' and their age is ' + age + ' and their hobby is ' + hobby + ' and their bedtime is ' + bedtime);
