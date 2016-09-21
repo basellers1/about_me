8f8739e32f0e24152f9177651d7037a8ed64a7d7
Britt Anne Sellers <basellers1@yahoo.com>
2016-09-20 17:22:22 -0700

    Changed line 17 to define useranswer

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+'use strict';
+//This is my first project. Hope it works!
+
 var username = prompt('What is your name?').toLowerCase();
   alert('Welcome, ' + username + '!');
     console.log('The user\'s name is ' + username);
@@ -10,8 +13,8 @@ var hello = prompt('Do you want to play a game?').toLowerCase();
   alert('Maybe another day.');
     console.log('Maybe another day.');
   }
-var q1 = prompt('I went to a private school in Hawaii, can you guess which one?');
-  if (useranswer = 'University of Hawaii'){
+var useranswer = prompt('I went to a private school in Hawaii, can you guess which one?');
+  if (useranswer === 'University of Hawaii'){
   alert('Sorry, that is not correct. UH is a public University');
     console.log('Sorry, that is not correct. UH is a public University');
   }
@@ -24,7 +27,7 @@ var q1 = prompt('I went to a private school in Hawaii, can you guess which one?'
     console.log('Sorry, that is not correct.  That must be a new one.');
 };
 
-var q2 = prompt('Memorable jobs for me were at Holland America Line, Hyatt Regency Waikiki. Can you guess my Major? ');
+var answer2 = prompt('Memorable jobs for me were at Holland America Line, Hyatt Regency Waikiki. Can you guess my Major? ');
   if (useranswer = 'Travel'){
     alert('Good guess!  Actually, I got my B.S.B.A. degree with a Travel Industry Management Major.');
     console.log('Good guess!  Actually, I got my B.S.B.A. degree with a Travel Industry Management Major.');
@@ -33,12 +36,30 @@ var q2 = prompt('Memorable jobs for me were at Holland America Line, Hyatt Regen
     alert('Good guess but that is not correct. Actually, I got my B.S.B.A. degree with a Travel Industry Management Major.');
     console.log('Good guess but that is not correct. Actually, I got my B.S.B.A. degree with a Travel Industry Management Major.');
 };
-var q3 = prompt('Do you know which forms of exercise I like?').toLowerCase();
+var answer3 = prompt('Do you know which forms of exercise I like?').toLowerCase();
   if (hello === 'Y' || hello === 'yes') {
   alert('That\'s fantastic!');
   console.log('That\'s fantastic!');
 }
-else{
+  else{
   alert('I wouldn\'t expect you to.  I like yoga and walking');
   console.log('I wouldn\'t expect you to.  I like yoga and walking');
+}
+var answer4 = prompt('Do you think I have pet?').toLowerCase();
+  if (useranswer === "N" || useranswer === "NO") {
+  alert('You are right!');
+  console.log('User guessed No');
+}
+  else {
+  alert('Not correct, sorry');
+  console.log('user guessed yes');
+}
+var answer4 = prompt('Do you think I like coding?').toUpperCase();
+  if (useranswer === 'Y' || useranswer === 'YES') {
+  alert('You are right!');
+  console.log('User guessed YES');
+}
+  else {
+  alert('Not correct, sorry');
+  console.log('user guessed NO');
 }
