42498a85e6091ce536e49d5fcf1cb41a03716c5e
Britt Anne Sellers <basellers1@yahoo.com>
2016-09-22 12:28:48 -0700

    Still fixing my game

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-//This is my first project. Hope it works!
+
 var numCorrect = 0;
 
 var userName = prompt('What is your name?').toLowerCase();
@@ -8,21 +8,21 @@ console.log('The user\'s name is ' + userName);
 
 var hello = prompt('Do you want to play a game?').toLowerCase();
 if (hello === 'y' || hello === 'yes') {
-  alert('That\'s fantastic!');
-  console.log('That\'s fantastic!');
+  alert('That\'s fantastic! Let\'s get started');
+  console.log('User wants to play');
 }
 else {
   alert('Maybe another day.');
   console.log('Maybe another day.');
 };
-var userAnswer = prompt('I went to a private school in Hawaii, can you guess which one?').toLowerCase();
-if (userAnswer === 'University of Hawaii'){
+var userAnswer = prompt('I went to a private school in Hawaii, can you guess which one?').toUpperCase();
+if (userAnswer === 'UNIVERSITY OF HAWAII' || userAnswer === 'UH'){
   alert('Sorry, that is not correct. UH is a public University');
-  console.log('Sorry, that is not correct. UH is a public University');
+  console.log('User guessed UH');
 }
-else if (userAnswer === 'Hawaii Pacific University') {
-  alert('Yes,that is the right answer');numCorrect++;
-  console.log('Yes, that is the right anwser');
+else if (userAnswer === 'HAWAII PACIFIC UNIVERSITY' || userAnswer === 'HPU') {
+  alert('Yes,that is the right answer.');numCorrect++;
+  console.log('User got it right');
 
 } else {
   alert('Sorry, that is not correct.  That must be a new one.');
@@ -32,21 +32,21 @@ else if (userAnswer === 'Hawaii Pacific University') {
 var userAnswer2 = prompt('Memorable jobs for me were at Holland America Line, Hyatt Regency Waikiki. Can you guess my Major? ');
 if (userAnswer2 === 'Travel'){
   alert('You are right!. I got my B.S.B.A. degree with a Travel Industry Management Major.');numCorrect++;
-  console.log('You are right! I got my B.S.B.A. degree with a Travel Industry Management Major.');
+  console.log('User figured it out');
 
 } else {
-  alert('Good guess but that is not correct. Actually, I got my B.S.B.A. degree with a Travel Industry Management Major.');
-  console.log('Good guess but that is not correct. Actually, I got my B.S.B.A. degree with a Travel Industry Management Major.');
+  alert('Actually, I got my B.S.B.A. degree with a Travel Industry Management Major.');
+  console.log('User guessed wrong');
 };
 
 var userAnswer3 = prompt('Do you know which forms of exercise I like?').toLowerCase();
-if (userAnswer3 === 'y' || userAnswer3 === 'yes') {
-  alert('That\'s fantastic!');numCorrect++;
-  console.log('That\'s fantastic!');
+if (userAnswer3 === 'y' || userAnswer3 === 'yes' || userAnswer3 === 'yoga' ) {
+  alert('Wow, that is a great guess!');numCorrect++;
+  console.log('User answered yes or correct');
 
 } else {
-  alert('I wouldn\'t expect you to.  I like yoga and walking');
-  console.log('I wouldn\'t expect you to.  I like yoga and walking');
+  alert('I wouldn\'t expect you to.  I like yoga');
+  console.log('User answered no');
 };
 
 var userAnswer4 = prompt('Do you think I have pet?').toUpperCase();
@@ -68,7 +68,7 @@ if (userAnswer5 === 'Y' || userAnswer5 === 'YES') {
   alert('You are not correct');
   console.log('user guessed NO');
 };
--->
+
 var correctAnswer = 9;
 for(var numGuess = 0; numGuess < 4; numGuess++){
   var userAnswer6 = parseInt(prompt('How many Pokeman did I catch yesterday?'));
@@ -81,16 +81,24 @@ for(var numGuess = 0; numGuess < 4; numGuess++){
     if(userAnswer6 < correctAnswer){
       alert('You guessed low. Try again');
       console.log('User guessed too low');
+    // } else if(userAnswer6 === 'string value'){
+    //   alert('That\'s a funny guess');
+    //   console.log('User didn\'t ;guess')
     } else {
       alert('You guessed too high. Try again');
       console.log('User guessed too low');
     }
   }
-}
-var Pokeman = ['Beedrill','Bulbasaur','Cubone','Dewgong','Krabby','Meowth','Oddish','Jiggypuff','Poliwag','Tentacool'];
-console.log(Pokeman.length);
-var userAnswer7 = prompt('Can you guess one of my favorites?');
-if(userAnswer7 === Pokeman.length);
-alert('Yes, I like that one');numCorrect++;;
+  var Pokeman = ['Beedrill','Bulbasaur','Cubone','Dewgong','Krabby','Meowth','Oddish','Jiggypuff','Poliwag','Tentacool'];
+  console.log(Pokeman.length);
+  var userAnswer7 = prompt('Can you guess one of my favorites?');
+  if(userAnswer7 === Pokeman.length){
+    alert('Yes, I like that one');numCorrect++;
+  } else {
+    alert('oops, that\'s not one I have');
+    console.log('User guessed one I don\'t have');
+  }
 
-console.log('User got ' + numCorrect + ' right');
+  console.log('User got ' + numCorrect + ' right');
+  alert('Congrats, you got ' + numCorrect + ' correct!');
+}
